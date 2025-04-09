733e4e96e50184b85162d7162e1ccac31c826aa5
VenomancerProMax <micomartija24@gmail.com>
2025-04-09 20:23:25 +0800

    add trigger

--- app/js/app.js
+++ app/js/app.js
@@ -76,7 +76,8 @@ function create_record(event) {
 
     ZOHO.CRM.API.insertRecord({
         Entity: "Applications1",
-        APIData: record_data
+        APIData: record_data,
+        Trigger: []
     }).then((response) => {
         const applicationData = response.data;
         applicationData.forEach((record) => {
